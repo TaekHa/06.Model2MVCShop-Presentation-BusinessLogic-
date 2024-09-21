a96124698a6a5847ea8a14eda9c31c6fe5bd2461
bitcamp <dbher.0010@gmail.com>
2024-09-21 12:22:03 +0900

    daliy commit : 20240921

--- 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/service/product/ProductService.java
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/main/java/com/model2/mvc/service/product/ProductService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.model2.mvc.service.product;
 
 import java.util.Map;
@@ -19,25 +18,3 @@ public interface ProductService {
 	public Product getProductByProdName(String prodName)throws Exception;
 
 }
-=======
-package com.model2.mvc.service.product;
-
-import java.util.Map;
-
-import com.model2.mvc.common.Search;
-import com.model2.mvc.service.domain.Product;
-
-public interface ProductService {
-	
-	public void addProduct(Product productVO) throws Exception;
-	
-	public Product getProduct(int prodNo) throws Exception;
-	
-	public Map<String , Object > getProductList(Search search) throws Exception;
-	
-	public void updateProduct(Product productVO) throws Exception;
-	
-	public Product getProductByProdName(String prodName)throws Exception;
-
-}
->>>>>>> refs/heads/test/dev
